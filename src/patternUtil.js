e14d891a89dc79ebb524e6ae94bea76440050991
naman Sharma <44020825+namankrs@users.noreply.github.com>
2018-10-30 18:30:59 +0530

    Introduced functions.
    Functions are:
    flipVertical()
    flipHorizontal()
    mergePatterns()
    composePatternsGenerator()

--- src/patternUtil.js
+++ src/patternUtil.js
@@ -13,4 +13,32 @@ const readUserInput = function(inputs){
   patternArgs = {type:typeOfPattern,width:inputs[3],height:inputs[4]}
   return patternArgs;
 }
-module.exports = {repeatCharacter,readUserInput};
+
+const flipVertical = function(pattern){
+  let flippedPattern = pattern.map(x=>x.split("").reverse().join(""));
+  return flippedPattern;
+}
+
+const flipHorizontal = function(pattern){
+  let flippedPattern = pattern.slice();
+  flippedPattern = flippedPattern.reverse();
+  return flippedPattern;
+}
+  
+
+const mergePatterns = function(firstPattern,secondPattern){
+  let mergedPatterns = [];
+  for(index in firstPattern){
+    mergedPatterns.push(firstPattern[index].concat(" ").concat(secondPattern[index]));
+  }
+  return mergedPatterns;
+}
+
+const composePatternsGenerator = function(func1,func2){
+  return function(args1,args2){
+    firstPattern = func1(args1);
+    secondPattern = func2(args2);
+    return mergePatterns(firstPattern,secondPattern);
+  }
+}
+module.exports = {repeatCharacter,readUserInput,composePatternsGenerator,flipHorizontal,flipVertical,mergePatterns};
--- test/patternUtilTest.js
+++ test/patternUtilTest.js
@@ -1,6 +1,6 @@
 const assert = require("assert");
 const patternUtil = require("../src/patternUtil.js");
-let {repeatCharacter,readUserInput} = patternUtil;
+let {repeatCharacter,readUserInput,flipVertical,flipHorizontal,mergePatterns} = patternUtil;
 
 {
   const repeatCharacterTest = function(width,character,expectedOutput){
@@ -16,6 +16,27 @@ let {repeatCharacter,readUserInput} = patternUtil;
   }
   testReadUserInput([1,2,'naman_sharma',4,5,6],{type:'naman',width:4,height:5});
   testReadUserInput([1,2,'naman',4],{type:'naman',width:4,height:undefined});
+}
+
+{
+  const testflipVertical = function(input,expectedOutput){
+    assert.deepEqual(flipVertical(input),expectedOutput);
+  }
+  testflipVertical(["*  ","** ","***"],["  *"," **","***"]);
+}
+
+{
+  const testflipHorizontal = function(input,expectedOutput){
+    assert.deepEqual(flipHorizontal(input),expectedOutput);
+  }
+  testflipHorizontal(["*  ","** ","***"],["***","** ","*  "]);
+}
+
+{
+  const testMergePatterns = function(firstPattern,secondPattern,expectedOutput){
+    assert.deepEqual(mergePatterns(firstPattern,secondPattern),expectedOutput);
+  }
+  testMergePatterns(["* ","**"],[" *","**"],["*   *","** **"]);
 }
 
 console.log("All tests passed");
